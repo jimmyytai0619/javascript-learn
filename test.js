cf3134ec3dc448835ae0172f13792c28259c64c0
jimmy <jimmytai0619@gmail.com>
2026-03-24 20:35:13 +0800

    test

---
+++ test.js
@@ -0,0 +1,14 @@
+let arr4 = [100,80,95,75,80];
+
+let a = arr4[0];
+let b = arr4[2];
+console.log("a :", a);
+console.log("b :", b);
+
+let arr5 = [1,2,3,4,5];
+
+let map1 = arr5.map( n => n*2);
+console.log("map 1 :", map1);
+
+let filter1 = arr5.filter( a => a>3);
+console.log("filter : ", filter1)
