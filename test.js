90ed31884b4886e514a7692ac3525d0b30559981
jimmy <jimmytai0619@gmail.com>
2026-03-28 22:48:54 +0800

    0.1

--- test.js
+++ test.js
@@ -12,3 +12,67 @@ console.log("map 1 :", map1);
 
 let filter1 = arr5.filter( a => a>3);
 console.log("filter : ", filter1)
+
+let info = {
+    name : "jordan",
+    height : 170,
+    weight : 56,
+    age : 26,
+    interest : ["basketball", "badminton", "swimming"]
+};
+
+console.log(info);
+
+info["interest"].push('bouldering');
+console.log(info.interest);
+
+
+// function
+// early function
+let add1 = function(a,b){
+    return a+b;
+};
+
+console.log(add1(3,5));
+
+//arrow
+
+let add2 = (a,b) => {
+    return a+b;
+}
+
+console.log( "add2 (6+ 6) = ", add2(6,6));
+
+let sayHello = (name) => {
+    console.log('My');
+    console.log('Name');
+    console.log('Is')
+    console.log(name);
+
+}
+
+sayHello("Kobe");
+
+
+let arr3 = [1,2,3,4,5];
+
+let map3 = arr3.map((n) => {
+    return n*3;
+});
+
+console.log("map 3 = ", map3);
+
+//function (global(全域) vs local(区域))
+
+//区域变数
+let test1 = () => {
+    var num1 = 12;
+    return num1;
+};
+
+let test2 = () => {
+    num2 = 18;
+    return num2;
+};
+
+console.log("num2 :", num2);
